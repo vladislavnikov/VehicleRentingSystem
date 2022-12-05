8d9f49509b133efa6a142f4f862cb8d07dea17bf
Vladislav <vladislav.d.nikov04@gmail.com>
2022-12-05 21:25:32 +0200

    tests

---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Tests
+{
+    public class Tests
+    {
+       
+
+        [Test]
+        public void Test1()
+        {
+            
+            Assert.Pass();
+        }
+    }
+}
